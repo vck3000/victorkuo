f6591969a2c55847001484ca7465eb68ab3f90ec
Victor Kuo <vck3000@gmail.com>
2020-09-21 14:13:57 +1000

    Deploy gh-pages

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const withOptimizedImages = require('next-optimized-images');
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = withOptimizedImages({
-  basePath: isProd ? '/victorkuo' : '',
-  assetPrefix: isProd ? '/victorkuo/' : '',
-});
